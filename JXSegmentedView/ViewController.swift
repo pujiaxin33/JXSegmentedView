9b954f2f2c8bf6ffde2189476ecce142f0955bb5
pujiaxin33 <317437084@qq.com>
2018-12-26 10:31:06 +0800

    Initial Commit

---
+++ JXSegmentedView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JXSegmentedView
+//
+//  Created by jiaxin on 2018/12/26.
+//  Copyright © 2018 jiaxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
